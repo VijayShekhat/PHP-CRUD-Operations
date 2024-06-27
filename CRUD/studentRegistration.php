858bf455c07b1f33d48ac24641297055a21d3ecf
VijayShekhat <109303751+VijayShekhat@users.noreply.github.com>
2024-06-27 08:15:01 +0530

    Update studentRegistration.php

--- CRUD/studentRegistration.php
+++ CRUD/studentRegistration.php
@@ -43,14 +43,6 @@
 								echo "<option>".$i."</option>";
 							}
 						?>
-						<!-- <option>1</option>
-						<option>2</option>
-						<option>3</option>
-						<option>4</option>
-						<option>5</option>
-						<option>6</option>
-						<option>7</option>
-						<option>8</option> -->
 					</select>
 				</td>
 			</tr>
